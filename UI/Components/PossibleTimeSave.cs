9e780dfee100cd1e1f37b3f20b1c1fd047423efc
Christopher Serr <cryze92@gmail.com>
2015-09-06 15:17:01 +0200

    Use C#6 and clean up

--- UI/Components/PossibleTimeSave.cs
+++ UI/Components/PossibleTimeSave.cs
@@ -16,15 +16,12 @@ namespace LiveSplit.UI.Components
         public PossibleTimeSaveSettings Settings { get; set; }
         private PossibleTimeSaveFormatter Formatter { get; set; }
 
-        public float PaddingTop { get { return InternalComponent.PaddingTop; } }
-        public float PaddingLeft { get { return InternalComponent.PaddingLeft; } }
-        public float PaddingBottom { get { return InternalComponent.PaddingBottom; } }
-        public float PaddingRight { get { return InternalComponent.PaddingRight; } }
+        public float PaddingTop => InternalComponent.PaddingTop;
+        public float PaddingLeft => InternalComponent.PaddingLeft;
+        public float PaddingBottom => InternalComponent.PaddingBottom;
+        public float PaddingRight => InternalComponent.PaddingRight;
 
-        public IDictionary<string, Action> ContextMenuControls
-        {
-            get { return null; }
-        }
+        public IDictionary<string, Action> ContextMenuControls => null;
 
         public PossibleTimeSave(LiveSplitState state)
         {
@@ -94,30 +91,19 @@ namespace LiveSplit.UI.Components
             InternalComponent.DrawHorizontal(g, state, height, clipRegion);
         }
 
-        public float VerticalHeight
-        {
-            get { return InternalComponent.VerticalHeight; }
-        }
+        public float VerticalHeight => InternalComponent.VerticalHeight;
 
-        public float MinimumWidth
-        {
-            get { return InternalComponent.MinimumWidth; }
-        }
+        public float MinimumWidth => InternalComponent.MinimumWidth;
 
-        public float HorizontalWidth
-        {
-            get { return InternalComponent.HorizontalWidth; }
-        }
+        public float HorizontalWidth => InternalComponent.HorizontalWidth;
 
-        public float MinimumHeight
-        {
-            get { return InternalComponent.MinimumHeight; }
-        }
+        public float MinimumHeight => InternalComponent.MinimumHeight;
 
         public string ComponentName
-        {
-            get { return (Settings.TotalTimeSave ? "Total " : "") + "Possible Time Save" + (Settings.Comparison == "Current Comparison" ? "" : " (" + CompositeComparisons.GetShortComparisonName(Settings.Comparison) + ")"); }
-        }
+        => (Settings.TotalTimeSave ? "Total " : "") + "Possible Time Save" 
+            + (Settings.Comparison == "Current Comparison" 
+                ? "" 
+                : " (" + CompositeComparisons.GetShortComparisonName(Settings.Comparison) + ")");
 
         public Control GetSettingsControl(LayoutMode mode)
         {
@@ -227,9 +213,6 @@ namespace LiveSplit.UI.Components
         {
         }
 
-        public int GetSettingsHashCode()
-        {
-            return Settings.GetSettingsHashCode();
-        }
+        public int GetSettingsHashCode() => Settings.GetSettingsHashCode();
     }
 }
--- UI/Components/PossibleTimeSaveFactory.cs
+++ UI/Components/PossibleTimeSaveFactory.cs
@@ -5,52 +5,20 @@ namespace LiveSplit.UI.Components
 {
     public class PossibleTimeSaveFactory : IComponentFactory
     {
-        public string ComponentName
-        {
-            get { return "Possible Time Save"; }
-        }
+        public string ComponentName => "Possible Time Save";
 
-        public string Description
-        {
-            get { return "Displays the difference between a comparison segment and the best segment, effectively showing how much time can be saved."; }
-        }
+        public string Description => "Displays the difference between a comparison segment and the best segment, effectively showing how much time can be saved.";
 
-        public ComponentCategory Category
-        {
-            get { return ComponentCategory.Information; }
-        }
+        public ComponentCategory Category => ComponentCategory.Information;
 
-        public IComponent Create(LiveSplitState state)
-        {
-            return new PossibleTimeSave(state);
-        }
+        public IComponent Create(LiveSplitState state) => new PossibleTimeSave(state);
 
-        public string UpdateName
-        {
-            get { return ComponentName; }
-        }
+        public string UpdateName => ComponentName;
 
-        public string XMLURL
-        {
-#if RELEASE_CANDIDATE
-            get { return "http://livesplit.org/update_rc_sdhjdop/Components/update.LiveSplit.PossibleTimeSave.xml"; }
-#else
-            get { return "http://livesplit.org/update/Components/update.LiveSplit.PossibleTimeSave.xml"; }
-#endif
-        }
+        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.PossibleTimeSave.xml";
 
-        public string UpdateURL
-        {
-#if RELEASE_CANDIDATE
-            get { return "http://livesplit.org/update_rc_sdhjdop/"; }
-#else
-            get { return "http://livesplit.org/update/"; }
-#endif
-        }
+        public string UpdateURL => "http://livesplit.org/update/";
 
-        public Version Version
-        {
-            get { return Version.Parse("1.6"); }
-        }
+        public Version Version => Version.Parse("1.6");
     }
 }
